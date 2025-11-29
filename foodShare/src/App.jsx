458a56c9dd458041ec0282c31bd1e902a7f2db01
tormenask <santiagoballa2003@gmail.com>
2025-11-29 10:48:41 -0500

    feat: configured the host with vite-plugin-federation

---
+++ foodShare/src/App.jsx
@@ -0,0 +1,15 @@
+/* import DeliveryTrackingWidget from 'foodShareGps/DeliveryTrackingWidget' */
+import GpsSimulator from 'widgetGPS/GpsSimulator'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <DeliveryTrackingWidget orderId="ABC-12345" /> */}
+      <GpsSimulator />
+    </>
+  )
+}
+
+export default App
---
+++ foodShare/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "foodShare",
+      filename: "remoteEntry.js",
+      exposes: {},
+      remotes: {},
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
